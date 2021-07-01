289755f1c7b211796a2a7651d51c047905a4f787
Reyes Salinas <kingsofsalt@gmail.com>
2021-07-01 15:34:59 -0500

    no message

---
+++ Engine/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Assets
+{
+    class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        
+        void Update()
+        {
+           transform.position = target.position;
+        }
+    }
+}
